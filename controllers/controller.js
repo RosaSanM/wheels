572ec9793e9f182738fe2f88574c36b098b89c48
mrosa_san <mrosa.san.mora@gmail.com>
2021-03-15 09:20:27 +0100

    cambios en carpeta controller

--- controllers/controller.js
+++ controllers/controller.js
@@ -3,12 +3,15 @@
 //Global
 var car;
 var wheels = new Array();
+var show;
 var cars = new Array();
 //Car elements   
-var newPlate = document.getElementById("plate").value;
+var newPlate;
 var newBrand;
 var newColor;
+var carTrue = false;
 //Wheels elements
+var wheelTrue = false;
 var diameter1;
 var brand1;
 var diameter2;
@@ -20,24 +23,27 @@ var brand4;
 //buttons
 var buttonCar = document.getElementById('car');
 var buttonWheel = document.getElementById('rueda');
-var inputs = (document.querySelectorAll("form-control"));
 //FUNCTIONS
-/*function validatePlate(){
-  let regexPlate = /[a-zA-Z]{4}[0-9]{3}$/;
-  plate = (document.getElementById("plate") as HTMLFormElement).value;
-  
-  if(!(regexPlate.test(plate.value))){
-    console.log('mal')
-    document.getElementById('plate')?.classList.add('is-invalid');
-  }else{
-    createCar();
-    vanish()
-  }
-}*/
 //Hidden form
-function vanish() {
+function hideCar() {
     var _a;
-    (_a = document.querySelector('.create-car')) === null || _a === void 0 ? void 0 : _a.classList.add('hidden');
+    if (carTrue) {
+        (_a = document.querySelector('.create-car')) === null || _a === void 0 ? void 0 : _a.classList.add('hidden');
+        carTrue = false;
+    }
+    else if (!carTrue) {
+        alert("rellena bien los campos");
+    }
+}
+function hideWheel() {
+    var _a, _b;
+    if (wheelTrue) {
+        (_a = document.querySelector('#wheels-form')) === null || _a === void 0 ? void 0 : _a.classList.add('hidden');
+        (_b = document.querySelector('#addCar')) === null || _b === void 0 ? void 0 : _b.classList.add('show');
+    }
+    else if (!wheelTrue) {
+        alert("rellena bien los campos");
+    }
 }
 //Create car
 function createCar() {
@@ -46,11 +52,16 @@ function createCar() {
     newBrand = document.getElementById("brand").value;
     newColor = document.getElementById("color").value;
     //second validate
-    if (typeof (newPlate) === 'string' && typeof (newBrand) === 'string' && typeof (newColor) === 'string') {
+    //if (typeof (newPlate) === 'string' && typeof (newBrand) === 'string' && typeof (newColor) === 'string') {
+    if (wheelTrue) {
         //create object and add to array
         car = new Car(newPlate, newColor, newBrand);
         createWheel();
         cars.push(car);
+        hideWheel();
+    }
+    else {
+        hideWheel();
     }
 }
 //Create wheel
@@ -74,26 +85,136 @@ function createWheel() {
     var wheel4 = new Wheel(diameter4, brand4);
     car.addWheel(wheel4);
 }
-//LISTENERS
-//validate first form
-inputs.forEach(function (input) {
-    input.addEventListener("keyup", validateForm);
-    input.addEventListener("blur", validateForm);
-});
-function validateForm() {
-    inputs.forEach(function (input) {
-        if (input.checkValidity() === false) {
-            input.classList.add('is-invalid');
-        }
-    });
+//VALIDATES
+//validate plate
+function validatePlate() {
+    var _a, _b, _c, _d;
+    var regexPlate = /[a-zA-Z]{4}[0-9]{3}$/;
+    newPlate = document.getElementById("plate").value;
+    if (!(regexPlate.test(newPlate))) {
+        (_a = document.getElementById('plate')) === null || _a === void 0 ? void 0 : _a.classList.remove('is-valid');
+        (_b = document.getElementById('plate')) === null || _b === void 0 ? void 0 : _b.classList.add('is-invalid');
+        carTrue = false;
+    }
+    else if (regexPlate.test(newPlate)) {
+        (_c = document.getElementById('plate')) === null || _c === void 0 ? void 0 : _c.classList.add('is-valid');
+        (_d = document.getElementById('plate')) === null || _d === void 0 ? void 0 : _d.classList.remove('is-invalid');
+        carTrue = true;
+    }
 }
+//Validate color
+function validateColor() {
+    var _a, _b, _c, _d;
+    newColor = document.getElementById("color").value;
+    if (newColor === "") {
+        (_a = document.getElementById('color')) === null || _a === void 0 ? void 0 : _a.classList.remove('is-valid');
+        (_b = document.getElementById('color')) === null || _b === void 0 ? void 0 : _b.classList.add('is-invalid');
+        carTrue = false;
+    }
+    else {
+        (_c = document.getElementById('color')) === null || _c === void 0 ? void 0 : _c.classList.remove('is-invalid');
+        (_d = document.getElementById('color')) === null || _d === void 0 ? void 0 : _d.classList.add('is-valid');
+        carTrue = true;
+    }
+}
+//Validate brand 
+function validateBrand() {
+    var _a, _b, _c, _d;
+    newBrand = document.getElementById("brand").value;
+    if (newBrand === "") {
+        (_a = document.getElementById('brand')) === null || _a === void 0 ? void 0 : _a.classList.remove('is-valid');
+        (_b = document.getElementById('brand')) === null || _b === void 0 ? void 0 : _b.classList.add('is-invalid');
+        carTrue = false;
+    }
+    else {
+        (_c = document.getElementById('brand')) === null || _c === void 0 ? void 0 : _c.classList.add('is-valid');
+        (_d = document.getElementById('brand')) === null || _d === void 0 ? void 0 : _d.classList.remove('is-invalid');
+        carTrue = true;
+    }
+}
+//Validate wheels
+function validateWheel(e) {
+    switch (e.target.name) {
+        case "diameter":
+            validateDiameter(e.target.value, e.target.id);
+            break;
+        case "brand":
+            validateEmpty(e.target.value, e.target.id);
+            break;
+    }
+    function validateDiameter(input, id) {
+        var _a, _b, _c, _d;
+        if (input < 0.4 || input > 2 || input == undefined) {
+            (_a = document.getElementById(id)) === null || _a === void 0 ? void 0 : _a.classList.add('is-invalid');
+            (_b = document.getElementById(id)) === null || _b === void 0 ? void 0 : _b.classList.remove('is-valid');
+            wheelTrue = false;
+        }
+        else {
+            (_c = document.getElementById(id)) === null || _c === void 0 ? void 0 : _c.classList.add('is-valid');
+            (_d = document.getElementById(id)) === null || _d === void 0 ? void 0 : _d.classList.remove('is-invalid');
+            wheelTrue = true;
+        }
+    }
+    function validateEmpty(input, id) {
+        var _a, _b, _c, _d;
+        if (input === "") {
+            (_a = document.getElementById("" + id)) === null || _a === void 0 ? void 0 : _a.classList.add('is-invalid');
+            (_b = document.getElementById("" + id)) === null || _b === void 0 ? void 0 : _b.classList.remove('is-valid');
+            wheelTrue = false;
+        }
+        else {
+            (_c = document.getElementById("" + id)) === null || _c === void 0 ? void 0 : _c.classList.add('is-valid');
+            (_d = document.getElementById("" + id)) === null || _d === void 0 ? void 0 : _d.classList.remove('is-invalid');
+            wheelTrue = true;
+        }
+    }
+}
+//LISTENERS
+//car button
 buttonCar.addEventListener('click', function (e) {
     e.preventDefault();
-    validatePlate();
+    //comprobar campos
+    hideCar();
     //llamar a validacion!!
 });
 //create object
+//wheel button
 buttonWheel.addEventListener('click', function (e) {
     e.preventDefault();
     createCar();
 });
+//validate inputs car
+document.getElementById("plate").addEventListener('keyup', validatePlate);
+document.getElementById("plate").addEventListener('blur', validatePlate);
+document.getElementById("color").addEventListener('keyup', validateColor);
+document.getElementById("color").addEventListener('blur', validateColor);
+document.getElementById("brand").addEventListener('keyup', validateBrand);
+document.getElementById("brand").addEventListener('blur', validateBrand);
+//validate inputs wheels
+//listeners diameter
+document.getElementById("diameter1").addEventListener('keyup', validateWheel);
+document.getElementById("diameter1").addEventListener('blur', validateWheel);
+document.getElementById("diameter2").addEventListener('keyup', validateWheel);
+document.getElementById("diameter2").addEventListener('blur', validateWheel);
+document.getElementById("diameter3").addEventListener('keyup', validateWheel);
+document.getElementById("diameter3").addEventListener('blur', validateWheel);
+document.getElementById("diameter4").addEventListener('keyup', validateWheel);
+document.getElementById("diameter4").addEventListener('blur', validateWheel);
+//listeners brand
+document.getElementById("brand1").addEventListener('keyup', validateWheel);
+document.getElementById("brand1").addEventListener('blur', validateWheel);
+document.getElementById("brand2").addEventListener('keyup', validateWheel);
+document.getElementById("brand2").addEventListener('blur', validateWheel);
+document.getElementById("brand3").addEventListener('keyup', validateWheel);
+document.getElementById("brand3").addEventListener('blur', validateWheel);
+document.getElementById("brand4").addEventListener('keyup', validateWheel);
+document.getElementById("brand4").addEventListener('blur', validateWheel);
+//let inputsDiameter = [(document.getElementsByClassName("diameter"))];
+/*let inputsLength = inputs.length;
+for(let element of inputsDiameter){
+  element.addEventListener()
+}
+
+for(let i = 0; i<=inputsLength; i++){
+  inputsDiameter[i].addEventListener()
+}*/
--- controllers/controller.ts
+++ controllers/controller.ts
@@ -1,13 +1,16 @@
 //VARIABLES
 //Global
-let car:Car;
-let wheels:Wheel[] = new Array();
-const cars = new Array(); 
+let car: Car;
+let wheels: Wheel[] = new Array();
+let show;
+const cars = new Array();
 //Car elements   
-let newPlate = (document.getElementById("plate") as HTMLFormElement).value;
+let newPlate;
 let newBrand;
 let newColor;
+let carTrue: boolean = false;
 //Wheels elements
+let wheelTrue: boolean = false;
 let diameter1;
 let brand1;
 let diameter2;
@@ -17,98 +20,211 @@ let brand3;
 let diameter4;
 let brand4;
 //buttons
-let buttonCar   = (document.getElementById('car') as HTMLElement); 
+let buttonCar   = (document.getElementById('car') as HTMLElement);
 let buttonWheel = (document.getElementById('rueda') as HTMLFontElement);
-let inputs = (document.querySelectorAll("form-control"));
+
 
 //FUNCTIONS
-/*function validatePlate(){ 
-  let regexPlate = /[a-zA-Z]{4}[0-9]{3}$/;
-  plate = (document.getElementById("plate") as HTMLFormElement).value;
-  
-  if(!(regexPlate.test(plate.value))){
-    console.log('mal')
-    document.getElementById('plate')?.classList.add('is-invalid');
-  }else{
-    createCar();
-    vanish()
-  }
-}*/
-
 //Hidden form
-function vanish(){
-  document.querySelector('.create-car')?.classList.add('hidden');
+function hideCar() {
+  if(carTrue){
+    document.querySelector('.create-car')?.classList.add('hidden');
+    carTrue = false;
+  }else if(!carTrue){
+    alert("rellena bien los campos");
+  }    
+}
+function hideWheel(){
+  if(wheelTrue){
+    document.querySelector('#wheels-form')?.classList.add('hidden');
+    document.querySelector('#addCar')?.classList.add('show');
+    
+  }else if(!wheelTrue){
+    alert("rellena bien los campos");
+  }
 }
 
 //Create car
-function createCar(){
+function createCar() {
   //get value of inputs
-    newPlate  = (document.getElementById("plate") as HTMLFormElement).value;
-    newBrand  = (document.getElementById("brand") as HTMLFormElement).value;
-    newColor  = (document.getElementById("color") as HTMLFormElement).value;
+  newPlate = (document.getElementById("plate") as HTMLFormElement).value;
+  newBrand = (document.getElementById("brand") as HTMLFormElement).value;
+  newColor = (document.getElementById("color") as HTMLFormElement).value;
+  
   //second validate
-    if(typeof(newPlate) === 'string' && typeof(newBrand) === 'string' && typeof(newColor) === 'string'){
+  //if (typeof (newPlate) === 'string' && typeof (newBrand) === 'string' && typeof (newColor) === 'string') {
+  if(wheelTrue){  
   //create object and add to array
-      car = new Car(newPlate,newColor, newBrand);
-      createWheel();
-      cars.push(car)
-    }
+    car = new Car(newPlate, newColor, newBrand);
+    createWheel();
+    cars.push(car)
+    hideWheel();
+    
+   
+  }else{
+    hideWheel();
+  }
 }
 //Create wheel
-function createWheel(){
+function createWheel() {
   //wheel values
-    let diameter1 = (document.getElementById('diameter1') as HTMLFormElement).value;
-    let brand1    = (document.getElementById('brand1') as HTMLFormElement).value;
-    let diameter2 = (document.getElementById('diameter2') as HTMLFormElement).value;
-    let brand2    = (document.getElementById('brand2') as HTMLFormElement).value;
-    let diameter3 = (document.getElementById('diameter3') as HTMLFormElement).value;
-    let brand3    = (document.getElementById('brand3') as HTMLFormElement).value;
-    let diameter4 = (document.getElementById('diameter4') as HTMLFormElement).value;
-    let brand4    = (document.getElementById('brand4') as HTMLFormElement).value;
-    
-    //create object and add
-    let wheel1:Wheel = new Wheel(diameter1, brand1);
-    car.addWheel(wheel1);
+  let diameter1 = (document.getElementById('diameter1') as HTMLFormElement).value;
+  let brand1    = (document.getElementById('brand1') as HTMLFormElement).value;
+  let diameter2 = (document.getElementById('diameter2') as HTMLFormElement).value;
+  let brand2    = (document.getElementById('brand2') as HTMLFormElement).value;
+  let diameter3 = (document.getElementById('diameter3') as HTMLFormElement).value;
+  let brand3    = (document.getElementById('brand3') as HTMLFormElement).value;
+  let diameter4 = (document.getElementById('diameter4') as HTMLFormElement).value;
+  let brand4    = (document.getElementById('brand4') as HTMLFormElement).value;
   
-    let wheel2:Wheel = new Wheel(diameter2, brand2);
-    car.addWheel(wheel2);
+  //create object and add
+  let wheel1: Wheel = new Wheel(diameter1, brand1);
+  car.addWheel(wheel1);
+
+  let wheel2: Wheel = new Wheel(diameter2, brand2);
+  car.addWheel(wheel2);
+
+  let wheel3: Wheel = new Wheel(diameter3, brand3);
+  car.addWheel(wheel3)
+
+  let wheel4: Wheel = new Wheel(diameter4, brand4);
+  car.addWheel(wheel4);
+
   
-    let wheel3:Wheel = new Wheel(diameter3, brand3);
-    car.addWheel(wheel3)
-    
-    let wheel4:Wheel = new Wheel(diameter4, brand4);
-    car.addWheel(wheel4);
 }
- 
+
+//VALIDATES
+//validate plate
+function validatePlate() {
+  let regexPlate = /[a-zA-Z]{4}[0-9]{3}$/;
+  newPlate = (document.getElementById("plate") as HTMLFormElement).value;
+
+  if (!(regexPlate.test(newPlate))) {
+    document.getElementById('plate')?.classList.remove('is-valid');
+    document.getElementById('plate')?.classList.add('is-invalid');
+    carTrue = false;
+  } else if (regexPlate.test(newPlate)) {
+    document.getElementById('plate')?.classList.add('is-valid');
+    document.getElementById('plate')?.classList.remove('is-invalid');
+    carTrue = true;
+  }
+}
+//Validate color
+function validateColor() {
+  newColor = (document.getElementById("color") as HTMLFormElement).value;
+  if (newColor === "") {
+    document.getElementById('color')?.classList.remove('is-valid');
+    document.getElementById('color')?.classList.add('is-invalid');
+    carTrue = false;
+  } else {
+    document.getElementById('color')?.classList.remove('is-invalid');
+    document.getElementById('color')?.classList.add('is-valid');
+    carTrue = true;
+  }
+}
+//Validate brand 
+function validateBrand() {
+  newBrand = (document.getElementById("brand") as HTMLFormElement).value;
+  if (newBrand === "") {
+    document.getElementById('brand')?.classList.remove('is-valid');
+    document.getElementById('brand')?.classList.add('is-invalid');
+    carTrue = false;
+  } else {
+    document.getElementById('brand')?.classList.add('is-valid');
+    document.getElementById('brand')?.classList.remove('is-invalid');
+    carTrue = true;
+  }
+}
+//Validate wheels
+function validateWheel(e: any) {
+  switch (e.target.name) {
+    case "diameter":
+      validateDiameter(e.target.value, e.target.id);
+      break
+    case "brand":
+      validateEmpty(e.target.value, e.target.id);
+      break
+  }
+
+  function validateDiameter(input: number, id:string) {
+    if (input < 0.4 || input > 2 || input == undefined) {
+      document.getElementById(id)?.classList.add('is-invalid');
+      document.getElementById(id)?.classList.remove('is-valid');
+      wheelTrue = false;
+    } else {
+      document.getElementById(id)?.classList.add('is-valid');
+      document.getElementById(id)?.classList.remove('is-invalid');
+      wheelTrue = true;
+    }
+  }
+
+  function validateEmpty(input: string, id: string) {
+    if (input === "") {
+      document.getElementById(`${id}`)?.classList.add('is-invalid');
+      document.getElementById(`${id}`)?.classList.remove('is-valid');
+      wheelTrue = false;
+    } else {
+      document.getElementById(`${id}`)?.classList.add('is-valid');
+      document.getElementById(`${id}`)?.classList.remove('is-invalid');
+      wheelTrue = true;
+    }
+  }
+}
 
 //LISTENERS
-//validate first form
-inputs.forEach((input) => {
-  input.addEventListener("keyup", validateForm);
-  input.addEventListener("blur", validateForm);
-});
-function validateForm(){
-  inputs.forEach((input)=>{
-    if (input.checkValidity() === false) {
-      input.classList.add('is-invalid')
-    }
-  });
-}
-
-buttonCar.addEventListener('click', (e)=>{
+//car button
+buttonCar.addEventListener('click', (e) => {
   e.preventDefault();
-  validatePlate();
+  //comprobar campos
+  hideCar();
   //llamar a validacion!!
-} );
+});
 //create object
-buttonWheel.addEventListener('click', (e)=>{
+//wheel button
+buttonWheel.addEventListener('click', (e) => {
   e.preventDefault();
   createCar();
-});
-
-
   
+});
+//validate inputs car
+(document.getElementById("plate") as HTMLFormElement).addEventListener('keyup', validatePlate);
+(document.getElementById("plate") as HTMLFormElement).addEventListener('blur', validatePlate);
+(document.getElementById("color") as HTMLFormElement).addEventListener('keyup', validateColor);
+(document.getElementById("color") as HTMLFormElement).addEventListener('blur', validateColor);
+(document.getElementById("brand") as HTMLFormElement).addEventListener('keyup', validateBrand);
+(document.getElementById("brand") as HTMLFormElement).addEventListener('blur', validateBrand);
+//validate inputs wheels
+//listeners diameter
+(document.getElementById("diameter1") as HTMLFormElement).addEventListener('keyup', validateWheel);
+(document.getElementById("diameter1") as HTMLFormElement).addEventListener('blur', validateWheel);
+(document.getElementById("diameter2") as HTMLFormElement).addEventListener('keyup', validateWheel);
+(document.getElementById("diameter2") as HTMLFormElement).addEventListener('blur', validateWheel);
+(document.getElementById("diameter3") as HTMLFormElement).addEventListener('keyup', validateWheel);
+(document.getElementById("diameter3") as HTMLFormElement).addEventListener('blur', validateWheel);
+(document.getElementById("diameter4") as HTMLFormElement).addEventListener('keyup', validateWheel);
+(document.getElementById("diameter4") as HTMLFormElement).addEventListener('blur', validateWheel);
+//listeners brand
+(document.getElementById("brand1") as HTMLFormElement).addEventListener('keyup', validateWheel);
+(document.getElementById("brand1") as HTMLFormElement).addEventListener('blur', validateWheel);
+(document.getElementById("brand2") as HTMLFormElement).addEventListener('keyup', validateWheel);
+(document.getElementById("brand2") as HTMLFormElement).addEventListener('blur', validateWheel);
+(document.getElementById("brand3") as HTMLFormElement).addEventListener('keyup', validateWheel);
+(document.getElementById("brand3") as HTMLFormElement).addEventListener('blur', validateWheel);
+(document.getElementById("brand4") as HTMLFormElement).addEventListener('keyup', validateWheel);
+(document.getElementById("brand4") as HTMLFormElement).addEventListener('blur', validateWheel);
 
 
+
+
+
+//let inputsDiameter = [(document.getElementsByClassName("diameter"))];
+/*let inputsLength = inputs.length;
+for(let element of inputsDiameter){
+  element.addEventListener()
+}
+
+for(let i = 0; i<=inputsLength; i++){
+  inputsDiameter[i].addEventListener()
+}*/
 
 
